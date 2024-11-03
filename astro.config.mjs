8620223b5d30bb1c12ca6fcdfcbbd664f389ec27
zettalyst <zettalyst@gmail.com>
2024-11-03 17:14:13 +0900

    refactor: css posting list

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zettalyst.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
