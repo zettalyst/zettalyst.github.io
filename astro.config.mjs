f52e031da4bf96810ebc65ef327752b1601e16ab
zettalyst <zettalyst@gmail.com>
2025-02-09 01:10:08 +0900

    chore: Update dependencies and Tailwind CSS configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zettalyst.github.io',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
