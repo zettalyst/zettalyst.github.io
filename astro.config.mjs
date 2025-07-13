3dbac7099c48d2180258fa9ad903303fe9a5e22f
zettalyst <zettalyst@gmail.com>
2025-07-13 14:27:53 +0900

    feat: create robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zettalyst.github.io',
   integrations: [mdx(), sitemap()],
-  trailingSlash: 'never',
 
   vite: {
     plugins: [tailwindcss()],
